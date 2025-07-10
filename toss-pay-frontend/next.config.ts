b1a493b7e56c6bb0126a13c0d02a0a23ed2a8fd6
lenac115 <lenac115@naver.com>
2025-07-10 14:27:36 +0900

    2025/07/10 CI CD 작업을 위한 커밋

--- toss-pay-frontend/next.config.ts
+++ toss-pay-frontend/next.config.ts
@@ -1,14 +1,7 @@
 import type { NextConfig } from "next";
-import path from "path";
 
 const nextConfig: NextConfig = {
-    webpack: (config) => {
-        config.resolve.alias = {
-            ...(config.resolve.alias || {}),
-            "@": path.resolve(__dirname, "src"),
-        };
-        return config;
-    },
+  /* config options here */
 };
 
 export default nextConfig;
