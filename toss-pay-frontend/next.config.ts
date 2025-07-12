b5bbae9ef7ec861d38fb27128d413352d231da79
창섭 <lenac115@naver.com>
2025-07-12 10:34:53 +0900

    2025/07/11 CI CD 작업을 위한 커밋

--- toss-pay-frontend/next.config.ts
+++ toss-pay-frontend/next.config.ts
@@ -4,6 +4,10 @@ const nextConfig: NextConfig = {
     eslint: {
         ignoreDuringBuilds: true,
     },
+    devServer: {
+        host: '0.0.0.0',
+        port: 3000,
+    },
 };
 
 export default nextConfig;
