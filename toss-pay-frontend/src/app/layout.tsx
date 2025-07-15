0824d5250efd0145afd5e5bc9a6512fe01f61af4
창섭 <lenac115@naver.com>
2025-07-15 19:37:44 +0900

    2025/07/15 CI CD 작업을 위한 커밋

--- toss-pay-frontend/src/app/layout.tsx
+++ toss-pay-frontend/src/app/layout.tsx
@@ -1,4 +1,4 @@
-import './utils/cryptoPolifill';
+import "../utils/cryptoPoly";
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
