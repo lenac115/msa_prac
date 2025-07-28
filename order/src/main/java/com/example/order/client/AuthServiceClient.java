ba4066ad5274c515cb16a3ffeb24755be5161601
창섭 <lenac115@naver.com>
2025-07-28 15:52:34 +0900

    2025/07/27 CI CD 작업을 위한 커밋

--- order/src/main/java/com/example/order/client/AuthServiceClient.java
+++ order/src/main/java/com/example/order/client/AuthServiceClient.java
@@ -4,7 +4,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
 
-@FeignClient(name = "auth-service", url = "http://auth-service:8081")
+@FeignClient(name = "auth-service", url = "${AZURE_HOST}")
 public interface AuthServiceClient {
 
     @GetMapping("/auth/common/get/buyer-id")
